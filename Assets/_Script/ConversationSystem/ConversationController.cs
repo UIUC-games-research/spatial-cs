246f8c1b41054048cc995ec4ee4506047fe710b3
Nick Olenz <nickolenz@comcast.net>
2016-07-05 12:49:04 -0500

    Proof-of-concept scene-juggling magic.

--- Assets/_Script/ConversationSystem/ConversationController.cs
+++ Assets/_Script/ConversationSystem/ConversationController.cs
@@ -51,6 +51,16 @@ public class ConversationController : MonoBehaviour
 	
 	void Update ()
 	{
+		//TODO REMOVE THIS DEBUG STUFF.
+		if (Input.GetKeyDown(KeyCode.B))
+		{
+			LoadUtils.BundleScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+		}
+		if (Input.GetKeyDown(KeyCode.N))
+		{
+			LoadUtils.LoadScene("construction", this);
+		}
+
 		// Ability to cancel a conversation.
 		if (Input.GetKeyDown(KeyCode.Escape) && currentEscRule)
 		{
---
+++ Assets/_Script/SceneManagement/LoadUtils.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LoadUtils : MonoBehaviour
+{
+	// Stores all scenes which have been loaded. Added to in BundleScene whenever a new bundle is created.
+	public static Dictionary<string, GameObject> loadedScenes = new Dictionary<string, GameObject>();
+
+	// Bundles a scene so it is entirely parented under an empty GameObject named
+	// the same way as the scene. This should be called when loading scenes to ensure
+	// everything stays bundled.
+	public static void BundleScene(string sceneName)
+	{
+		// Create empty dummy, tagged as "SceneBundle"
+		if (loadedScenes.ContainsKey(sceneName))
+		{
+			return;
+		}
+		GameObject sceneBundle = new GameObject(sceneName);
+		loadedScenes.Add(sceneName, sceneBundle);
+		sceneBundle.tag = "SceneBundle";
+
+		// Parent all root objects to it.
+		object[] objs = FindObjectsOfType(typeof(GameObject));
+		foreach (object ii in objs)
+		{
+			GameObject gg = (GameObject)ii;
+			Transform tt = gg.transform;
+			if (tt.parent == null && gg.tag != "SceneBundle")
+			{
+				tt.parent = sceneBundle.transform;
+			}
+		}
+
+		// If nothing got parented for some reason, delete it.
+		if (sceneBundle.transform.childCount == 0)
+		{
+			Destroy(sceneBundle);
+		}
+	}
+
+	public static void LoadScene(string sceneName, MonoBehaviour caller)
+	{
+		// Ensure scene is already bundled.
+		BundleScene(SceneManager.GetActiveScene().name);
+
+		// Get scene ref.
+		Scene current = SceneManager.GetActiveScene();
+
+		// Load the scene we are trying to merge.
+		Debug.Log("Running Function.");
+		GameObject dummy = new GameObject();
+		caller.StartCoroutine(Loader(sceneName,current));
+	}
+
+	static IEnumerator Loader(string sceneName, Scene current)
+	{
+		// Load.
+		Debug.Log("Loading");
+		SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+		
+		// Wait for it to load.
+		yield return null;
+		Debug.Log("Waited one frame");
+
+		// Merge.
+		Scene toLoad = SceneManager.GetSceneByName(sceneName);
+		SceneManager.MergeScenes(toLoad, current);
+
+		// Bundle.
+		BundleScene(sceneName);
+
+		// Deactivate current scene.
+		loadedScenes[current.name].SetActive(false);
+
+		// Give cursor back.
+		Cursor.visible = true;
+		Cursor.lockState = CursorLockMode.None;
+	}
+}
