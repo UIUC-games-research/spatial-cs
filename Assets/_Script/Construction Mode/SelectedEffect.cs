c959a0fe72d78080b074b0f9d305327ee23baded
Nick Olenz <nickolenz@comcast.net>
2016-07-07 16:15:22 -0500

    Struggling with face selection stuff... Attempted something, but had to comment it all out because of some crazy nonsense which I now need to look into. Not sure what the final effect will be at this point.

---
+++ Assets/_Script/Construction Mode/SelectedEffect.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectedEffect : MonoBehaviour {
+
+	float delay = 0.5f;
+	float timer = 0f;
+	public RaycastHit hitInfo;      // Set by SelectPart when this script is applied.
+	GameObject instance;
+
+	void Start ()
+	{
+		SpawnGhost();
+	}
+	
+	void Update ()
+	{
+		/*
+		timer += Time.deltaTime;
+		if (timer > delay)
+		{
+			timer = 0f;
+			SpawnGhost();
+		}
+		*/
+	}
+
+	public void SpawnGhost()
+	{
+		Debug.Log("SPAWN");
+		instance = Instantiate(gameObject);
+		Destroy(instance.GetComponent<SelectedEffect>());
+		instance.transform.position = transform.position;
+		instance.transform.localScale = 11 * transform.localScale;
+		instance.transform.rotation = transform.rotation;
+		instance.transform.SetParent(transform.parent);
+
+		SelectedGhost ghost = instance.AddComponent<SelectedGhost>();
+		ghost.hitInfo = hitInfo;
+	}
+
+	void OnDestroy()
+	{
+		Destroy(instance);
+	}
+}
---
+++ Assets/_Script/Construction Mode/SelectedGhost.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectedGhost : MonoBehaviour {
+
+	float timer = 0f;
+	float destroyAt = 1f;
+	public RaycastHit hitInfo;		// Set on creation by SelectedEffect.
+
+	void Start ()
+	{
+		// Clean up the ghost.
+		SelectBehavior sel = GetComponent<SelectBehavior>();
+		FuseBehavior fus = GetComponent<FuseBehavior>();
+		SelectedEffect eff = GetComponent<SelectedEffect>();
+
+		Destroy(sel);
+		Destroy(fus);
+		Destroy(eff);
+
+		// Move up a little.
+		transform.position += (hitInfo.normal);
+	}
+	
+	void Update ()
+	{
+		/*
+		// Move and delete after some time.
+		timer += Time.deltaTime;
+		if (timer > destroyAt)
+		{
+			Debug.Log("Destroying");
+			Destroy(this.gameObject);
+		}
+		transform.position += (0.1f * hitInfo.normal);
+		*/
+	}
+}
