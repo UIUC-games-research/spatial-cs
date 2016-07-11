1cc620460017acce4988e126fbfa408bd47e202e
Nick Olenz <nickolenz@comcast.net>
2016-07-11 15:10:13 -0500

    Ghosts work, and not even very hacky! Remaining issues: Don't delete when fusing, can select backfaces, backfaces of ghosts are invisible.

--- Assets/_Script/Construction Mode/SelectedEffect.cs
+++ Assets/_Script/Construction Mode/SelectedEffect.cs
@@ -1,65 +1,73 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SelectedEffect : MonoBehaviour {
+public class SelectedEffect : MonoBehaviour
+{
 
 	float delay = 0.15f;
 	float timer = 0f;
 	public RaycastHit hitInfo;      // Set by SelectPart when this script is applied.
 	public GameObject selected;
 	GameObject instance;
+	GameObject hitCaster;
 
-	void Start ()
+	Vector3 currentRot;
+	Vector3 previousRot;
+	Vector3 normal;
+
+	void Start()
 	{
-		//SpawnGhost();
+		currentRot = transform.parent.localEulerAngles;
+		previousRot = transform.parent.localEulerAngles;
+		normal = hitInfo.normal;
+
+		// Also add a hitcaster object to keep our normal updated.
+		hitCaster = new GameObject();
+		hitCaster.transform.position = transform.position;
+		hitCaster.transform.localScale = 10 * transform.localScale;
+		hitCaster.transform.rotation = transform.rotation;
+		hitCaster.transform.parent = transform.parent;
+		hitCaster.transform.position += (20f * hitInfo.normal);
 	}
-	
-	void Update ()
+
+	void FixedUpdate()
 	{
-		
+
 		timer += Time.deltaTime;
 		if (timer > delay)
 		{
 			timer = 0f;
 			SpawnGhost();
 		}
-		
+
+		// Update the normal with the hitCaster.
+		Physics.Raycast(hitCaster.transform.position, transform.position - hitCaster.transform.position, out hitInfo);
+		//Debug.DrawRay(hitCaster.transform.position, 10 * (transform.position - hitCaster.transform.position), Color.cyan, 3f);
+
 	}
 
 	public void SpawnGhost()
 	{
-		//Debug.Log("SPAWN");
-		/*
-		instance = Instantiate(gameObject);
-		Destroy(instance.GetComponent<SelectedEffect>());
-		instance.transform.position = transform.position;
-		instance.transform.localScale = 10 * transform.localScale;
-		instance.transform.rotation = transform.rotation;
-		instance.transform.SetParent(transform.parent);
-		*/
-
 		// Transforms.
 		instance = new GameObject();
-		//instance = Instantiate(Resources.Load("MarkerSphere") as GameObject);
-		
+
 		instance.transform.position = transform.position;
 		instance.transform.localScale = 10 * transform.localScale;
 		instance.transform.rotation = transform.rotation;
-		instance.transform.parent = transform.parent;
+		//instance.transform.parent = transform.parent;
+		LoadUtils.InstantiateParenter(instance);
 		instance.layer = 2;
 
 		// Add mesh filter and renderer
-		
 		MeshFilter meshf = instance.AddComponent<MeshFilter>();
 		meshf.mesh = GetComponent<MeshFilter>().mesh;
 		MeshRenderer meshr = instance.AddComponent<MeshRenderer>();
 		meshr.material = Resources.Load("Opacity") as Material;
-		
+
 
 		// Add ghost script.
 		SelectedGhost ghost = instance.AddComponent<SelectedGhost>();
 		ghost.hitInfo = hitInfo;
-		ghost.selected = selected;
 	}
 
 	void OnDestroy()
--- Assets/_Script/Construction Mode/SelectedGhost.cs
+++ Assets/_Script/Construction Mode/SelectedGhost.cs
@@ -7,23 +7,10 @@ public class SelectedGhost : MonoBehaviour {
 	float destroyAt = 1f;
 	public RaycastHit hitInfo;      // Set on creation by SelectedEffect.
 	Vector3 normal;
-	public GameObject selected;		// As above.
-	Mesh mesh;
-	//Vector3 currRot = Vector3.zero;
-	//Vector3 startRot = Vector3.zero;
-	Quaternion currRot = Quaternion.identity;
-	Quaternion startRot = Quaternion.identity;
-	Vector3 initialFaceNormal;
 
 	void Start ()
 	{
-		mesh = hitInfo.transform.GetComponent<MeshFilter>().mesh;
 		normal = hitInfo.normal;
-		currRot = transform.parent.rotation;
-		startRot = transform.parent.rotation;
-		initialFaceNormal = mesh.normals[0];
-
-		FindHitFace();
 	}
 	
 	void FixedUpdate ()
@@ -38,24 +25,5 @@ public class SelectedGhost : MonoBehaviour {
 		}
 
 		transform.position += (0.08f * normal);
-
-		// Update normal direction
-		currRot = transform.parent.localRotation;
-		//Quaternion temp = currRot * Quaternion.Inverse(startRot);
-
-		// Rotate.
-		//normal = Quaternion.Euler(currRot.eulerAngles) * hitInfo.normal;
-		//normal = Quaternion.Euler(-1 * startRot.eulerAngles) * normal;
-		//normal = Quaternion.Euler(transform.parent.localEulerAngles) * initialFaceNormal;
-		//normal = Quaternion.Euler(transform.localEulerAngles) * normal;
-		//normal = initialFaceNormals
-		Debug.DrawLine(transform.position, transform.position + (5f * normal), Color.green, 3f);
-	}
-
-	void FindHitFace()
-	{
-		//Vector3[] normals = mesh.normals;
-		//Debug.Log("FACE: " + normals[0]);
-		//Debug.Log("HIT: " + hitInfo.normal);
 	}
 }
