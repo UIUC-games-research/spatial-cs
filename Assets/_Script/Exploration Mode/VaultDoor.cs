80dad1cc0a0ec9627381ad264febaca908be1d3a
Nick Olenz <nickolenz@comcast.net>
2016-08-01 16:21:42 -0500

    Vault door and trigger to enter vault.

---
+++ Assets/_Script/Exploration Mode/VaultDoor.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VaultDoor : MonoBehaviour
+{
+	public string tokenToCheck = "";
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player" && checkToken())
+		{
+			StartCoroutine(rotate(90f));
+		}
+	}
+	void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "Player" && checkToken())
+		{
+			StartCoroutine(rotate(-90f));
+		}
+	}
+	bool checkToken()
+	{
+		return ConversationTrigger.GetToken(tokenToCheck);
+	}
+	IEnumerator rotate(float amount)
+	{
+		for (int i = 0; i < 60; i++)
+		{
+			transform.Rotate(0f, amount / 60f, 0f, Space.World);
+			yield return null;
+		}
+	}
+}
